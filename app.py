f1efe54e25b9cc7b496883d914839f3c383f8b45
MaorEi <maor.eitan.dev@gmail.com>
2022-11-15 15:23:52 +0200

    adding abortions and error handling flask_smorest

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import uuid
 
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -12,11 +13,30 @@ def get_stores():
     # stores.values() isn't a list it cannot turnned into json, therefore we convert to list.
 
 
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure 'name' is included in the JSON payload")
+    for store in stores.values():
+        if store["name"] == store_data["name"]:
+            abort(400, "Store already exist.")
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
+
+
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+    if "price" not in item_data or "store_id" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload")
+    for item in items.values():
+        if item_data["name"] == item["name"] and item_data["store_id"] == item["store_id"]:
+            abort(400, "Item already exists.")
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 400
+        abort(404, message="Store not found.")
     item_id = uuid.uuid4().hex
     item = {**item_id, "id": item_id}
     items[item_id] = item
@@ -28,7 +48,7 @@ def get_store(store_id):
     try:
         return stores[store_id], 200
     except KeyError:
-        return {"message": "store doesn't exist"}, 400
+        abort(404, message="Store not found.")
 
 
 @app.get("/item/<string:item_id>")
@@ -36,18 +56,9 @@ def get_item(item_id):
     try:
         return items[item_id], 200
     except KeyError:
-        return {"message": "item doesn't exist"}, 400
+        abort(404, message="Item not found.")
+
 
 @app.get("/item")
 def get_all_items():
     return {"items": list(items.values())}
-
-
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    store_id = uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-    return store, 201
-
