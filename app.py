bdbe251a6855ae35ba24ea37a336d64420d7b4e2
MaorEi <maor.eitan.dev@gmail.com>
2022-11-16 10:20:50 +0200

    Adding delete & update items and delete store endpoints.

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ from db import items, stores
 app = Flask(__name__)
 
 
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id], 200
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
 @app.get("/store")
 def get_stores():
     return {"stores": list(stores.values())}  # by default returns 200
@@ -27,6 +35,31 @@ def create_store():
     return store, 201
 
 
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        for item in list(items.values()):  # can't delete values from dictionary during iteration
+            if item["store_id"] == store_id:
+                del items[item["id"]]
+        return {"message": "Store deleted"}
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id], 200
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
@@ -43,22 +76,23 @@ def create_item():
     return item, 201
 
 
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload")
     try:
-        return stores[store_id], 200
-    except KeyError:
-        abort(404, message="Store not found.")
-
-
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id], 200
+        item = items[item_id]
+        item |= item_data
+        return item
     except KeyError:
         abort(404, message="Item not found.")
 
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
